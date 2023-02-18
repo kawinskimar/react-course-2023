aa833d20180b0a73d84cf2a7dd261290f4f32e42
Marissa Kawinski <marissakawinski@gmail.com>
2023-02-18 12:28:32 -0500

    Initial commit

---
+++ p1-info-site/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <h1>Hello from React!</h1>
+      <ul>
+        <li>This is cool.</li>
+        <li>This is great!</li>
+      </ul>
+    </div>
+  )
+}
+
+export default App
